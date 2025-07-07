8173deed9004d892b632ae3211a6c68214a9ef6f
Ishwor Khadka <ishwork28@gmail.com>
2025-07-07 17:26:35 +0545

    Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,9 @@ import Pricing from '@/components/home/Pricing';
 import Header from '@/components/common/Header';
 import Footer from '@/components/common/Footer';
 import ChatBot from '@/chatbot/page';
+import Team from '@/components/home/Team';
+import Tools from '@/components/home/Tools';
+import CaseStudies from '@/components/home/CaseStudies';
 
 export default function Home() {
   return (
@@ -14,6 +17,9 @@ export default function Home() {
       <About />
       <Services />
       <Pricing />
+      <Team />
+      <CaseStudies />
+      <Tools />
       <ChatBot />
       <Footer />
     </main>
